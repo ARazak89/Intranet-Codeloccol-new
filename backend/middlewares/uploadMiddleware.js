4ee61c234f1e29189096653524d00b31ef8a5d10
yacoubou-seidou <yacoseidou@gmail.com>
2025-10-03 17:04:27 +0100

    remove old picture when update

--- backend/middlewares/uploadMiddleware.js
+++ backend/middlewares/uploadMiddleware.js
@@ -1,34 +1,54 @@
 import multer from "multer";
 import path from "path";
 import { fileURLToPath } from "url";
-
+import User from '../models/User.js';
+import fs from "fs";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Configuration du stockage de Multer
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    // Le dossier où les images seront stockées. Assurez-vous qu'il existe !
-    // Dans un environnement de production, vous utiliseriez un service de stockage cloud (S3, Cloudinary, etc.)
     cb(null, path.join(__dirname, "../public/uploads/profile_pictures"));
   },
-  filename: (req, file, cb) => {
-    // Nom du fichier : user ID + horodatage + extension originale
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(
-      null,
-      req.user._id + "-" + uniqueSuffix + path.extname(file.originalname),
-    );
+  filename: async (req, file, cb) => {
+    try {
+      const userId = req.user._id;
+      const user = await User.findById(userId);
+
+      // Vérifier si l'utilisateur a déjà une photo de profil
+      if (user && user.profilePicture) {
+        const oldPath = path.join(
+          __dirname,
+          "../public/uploads/profile_pictures",
+          path.basename(user.profilePicture), // on récupère juste le nom du fichier
+        );
+
+        // Vérifie si le fichier existe et le supprime
+        if (fs.existsSync(oldPath)) {
+          fs.unlinkSync(oldPath);
+          console.log(`Ancienne photo supprimée : ${oldPath}`);
+        }
+      }
+
+      // Nouveau nom de fichier
+      const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+      cb(null, userId + "-" + uniqueSuffix + path.extname(file.originalname));
+    } catch (err) {
+      cb(err);
+    }
   },
 });
 
 // Filtre pour n'accepter que les images
-const fileFilter = (req, file, cb) => {
+const fileFilter = async (req, file, cb) => {
   if (file.mimetype.startsWith("image/")) {
-    cb(null, true);
+    const userId = req.user._id;
+    const user = await User.findById(userId);
+    await cb(null, true);
     console.log('storage',storage.getFilename);
   } else {
-    cb(new Error("Seules les images sont autorisées !"), false);
+    await cb(new Error("Seules les images sont autorisées !"), false);
   }
 };
 
